8a6b0629d6c553f3ddff7a423d55d700809aedc4
JanethL <43326723+JanethL@users.noreply.github.com>
2021-05-02 15:58:51 -0700

    Update tetris.js

--- docs/tetris.js
+++ docs/tetris.js
@@ -1,27 +1,39 @@
-const canvas = document.getElementById('tetris');
+const canvas = document.getElementById('tetris'); //access canvas
 const ctx = canvas.getContext('2d');
 const button = document.getElementById('start');
-const resetButton = document.getElementById('reset');
-ctx.scale(20,20); // used to scale everything on canvas by 20
+const resetButton = document.getElementById('reset'); //grab reset button
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("instructions");
+const span = document.getElementsByClassName("close")[0];
 
+ctx.scale(20,20); //scale everything on canvas by 20
 
-//add event listener to start button so that when clicked the game is rendered 
-document.addEventListener("DOMContentLoaded", function (e) {
-    button.addEventListener('click', () => {
-            //we remove button to prevent another block to be rendered
-            button.style.display = 'none';
-    
-    resetButton.addEventListener('click', () => {
-        console.log("clicked!") // check  to see if eventListener is working
-        location.reload();
-        return false
-        
+//add event listener to start button so that when clicked the game is rendered
+button.addEventListener('click', () => { 
+        update(); // start the game by calling our update() function
+button.style.display = 'none'; //remove button to prevent another block from being rendered
+
+resetButton.addEventListener('click', () => { //add another event listener for when reset button is clicked
+    location.reload();
     })
+})
 
-    
+btn.addEventListener('click', () => { 
+    modal.style.display = "block";
+  })
+span.addEventListener('click', () => { 
+    modal.style.display = "none";
+  })
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+// Pick one block piece to start off drawing the entire structure and functions of game
 
 const blocks = [
-    //we include four rows to make rotation easier
+    //include four rows to make rotation easier
     [0, 0, 0],
     [0, 1, 1],
     [1, 1, 0],
@@ -50,9 +62,9 @@ function rotate(blocks, control) {
 }
 
 
-// we need to create a function to help us save all the pieces
+// create a function to save all the pieces
 function createBlocks(w, h){
-    const blocks = []; // we set an empty array
+    const blocks = []; // set an empty array
     while (h--){ // we know the height is 2 so here we are saying that while the height is not zero decrease h by 1 
         blocks.push(new Array(w).fill(0));
     }
@@ -90,9 +102,12 @@ function collide(field, player){
 function draw (){
     ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
+
+
     drawBlocks(field, {x:0, y:0}); // when we open our console to check the field `console.log(field);` we notice our zeros are being populated so we just need to draw those 
     drawBlocks(player.blocks, player.pos); 
+
+
 }
 
 function drawBlocks(blocks, offset){
@@ -221,7 +236,7 @@ document.addEventListener('keydown', e => {
         playerRotate(+1);
      while(collide(field, player)){  //when we rotate our blocks it exits the canvas walls so we need to check collison  everytime we rotate
          player.pos.x += offset; 
-         offset = -(offset + (offset > 0 ? 1 : -1)); //
+         offset = -(offset + (offset > 0 ? 1 : -1)); 
          if(offset > player.blocks[0].length){
              rotate(player.blocks, control);
             player.pos.x = pos; 
@@ -231,11 +246,3 @@ document.addEventListener('keydown', e => {
  }
  console.log(field); console.table(field);
 });
-
-
-
-update(); //we start the game with update
-
-})});
-
-// now I need to figure out why 
